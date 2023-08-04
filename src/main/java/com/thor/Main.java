c9f05090fd4ddcd3b8a2499b2a63fa0c613c26d0
ThorGalli <gallithor@gmail.com>
2023-08-04 18:18:31 -0300

    feat: add route for creating and reading customers

--- src/main/java/com/thor/Main.java
+++ src/main/java/com/thor/Main.java
@@ -1,19 +1,46 @@
 package com.thor;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class Main {
+@RequestMapping("api/v1/customers")
+public class Main { 
+
+  private final CustomerRepository customerRepository;
+
+  public Main(CustomerRepository customerRepository) {
+    this.customerRepository = customerRepository;
+  }
+
   public static void main(String[] args) {
     SpringApplication.run(Main.class, args);
   }
 
-  @GetMapping("/")
-  public String greet() {
-    return "Hello Springboot!";
+  @GetMapping
+  public List<Customer> getCustomers() {
+    return customerRepository.findAll();
+  }
+
+  record NewCustomerRequest(String name, String email, Integer age){}
+
+  @PostMapping
+  public void addCustomer(@RequestBody NewCustomerRequest request) {
+    Customer customer = new Customer();
+
+    customer.setName(request.name());
+    customer.setAge(request.age());
+    customer.setEmail(request.email());
+
+    customerRepository.save(customer);
+    
   }
 }
